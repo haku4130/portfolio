82c924af99fae635e2eb5a4a100774f94a2db11e
Andrey Osipov <a.osipov.code@gmail.com>
2026-07-08 11:52:39 +0300

    Use SVG-only favicon so Chrome stops preferring the old .ico

--- app/app.vue
+++ app/app.vue
@@ -12,8 +12,7 @@ useHead({
     { key: 'theme-color', name: 'theme-color', content: color }
   ],
   link: [
-    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', sizes: 'any' }
   ],
   htmlAttrs: {
     lang: locale
